99d2111aa6fc1669a29fbff72bc4ebdfa8219027
patrykbrzozowski <patrykbrzozula@gmail.com>
2024-11-09 22:12:07 +0100

    Fix mobile view

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IsActiveMatchOptions } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  public linkActiveOptions: IsActiveMatchOptions = {
+    matrixParams: 'exact',
+    queryParams: 'exact',
+    paths: 'exact',
+    fragment: 'exact',
+  };
+
   isMenuCollapsed: boolean = true;
 }
